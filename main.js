db0a7f6f391c59ef1c9e0404c6b1834e39e5bab1
ogabeeek <101085164+ogabeeek@users.noreply.github.com>
2022-05-26 19:55:03 +0500

    initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+var salaries={
+    Akmal:1200,
+    Salim:5200,
+    Karima:1800,
+    }
+    
+    var sum=0;
+    
+    
+    for(i in salaries){
+        sum += salaries[i]
+    }
+    
+    console.log(sum);
